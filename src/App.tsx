f3fb537a53c51f6733f2904a397e505cf706b3ec
Christian Bernecker <christian.bernecker@gmail.com>
2025-04-16 22:07:39 +0200

    fix: Entferne Import und Route für gelöschte Legal-Komponente aus App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import JsonToolsApp from './components/JsonToolsApp';
 import NotFound from './components/NotFound';
-import Legal from './components/Legal';
 import './App.css';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/json-explorer" element={<JsonToolsApp />} />
-        <Route path="/json-explorer/legal/:page" element={<Legal />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
