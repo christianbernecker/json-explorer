e1f03fb6c22bf22e7bee55902e5735218dbdc0be
Christian Bernecker <christian.bernecker@gmail.com>
2025-04-16 17:26:44 +0200

    feat: Add static contact page and update routing

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -28,6 +28,12 @@ const Footer: React.FC = () => {
             >
               Imprint
             </Link>
+            <a
+              href="/contact"
+              className="text-sm text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white"
+            >
+              Contact
+            </a>
           </div>
         </div>
       </div>
