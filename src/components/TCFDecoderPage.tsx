60a49a69ce64235a6d5a67f8543bff51e1774b1b
Christian Bernecker <christian.bernecker@gmail.com>
2025-05-19 09:19:20 +0200

    Fix TCF-Decoder-Props

--- src/components/TCFDecoderPage.tsx
+++ src/components/TCFDecoderPage.tsx
@@ -1,1 +1,1 @@
-import React from 'react'; const TCFDecoderPage = () => { return <div className='p-4'><h2>TCF Decoder</h2><p>Currently under maintenance.</p></div>; }; export default TCFDecoderPage;
+import React from 'react'; const TCFDecoderPage = ({ isDarkMode, toggleDarkMode }) => { return <div className='p-4'><h2>TCF Decoder</h2><p>Currently under maintenance.</p></div>; }; export default TCFDecoderPage;
