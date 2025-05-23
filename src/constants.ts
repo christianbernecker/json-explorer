63787dcd1fece5f0cd543950adb93ae96508e665
Christian Bernecker <christian.bernecker@gmail.com>
2025-05-23 20:59:04 +0200

    Version 2.0.0 für Produktions-Release vorbereiten

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 // App Version Constants
-export const APP_VERSION = 'v1.1.4';
-export const APP_VERSION_NEXT = 'v1.1.5';
+export const APP_VERSION = 'v2.0.0';
+export const APP_VERSION_NEXT = 'v2.0.1';
 export const APP_ENV = process.env.NODE_ENV === 'production' ? 'production' : 'development';
